902e2252b76e677d2ac36c71c6260c3523b3a9df
vvsrudra <vvsrudra@gmail.com>
2026-05-11 00:31:17 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,8 @@
+num1 = float(input("enter first number "))
+num2 = float(input("enter second number"))
+print("adiition",(num1 +num2) )
+print("substraction ",(num1-num2))
+print("multiplication", (num1*num2))
+print("division",(num1/num2))
+
+                  
